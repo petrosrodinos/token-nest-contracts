5e085e3322a6222d85519a8f200d62c0fc849355
petrosrodinos <petros.rodinos@yahoo.com>
2025-01-27 14:15:07 +0200

    updated

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity 0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
@@ -13,7 +13,7 @@ contract Token is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner, uint256 initialSupply, string memory name, string memory symbol)
         ERC20(name, symbol)
         Ownable(initialOwner)
-        ERC20Permit("Token")
+        ERC20Permit(name)
     {
         _mint(address(this), initialSupply * 10 ** decimals());
     }
--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,32 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity 0.8.28;
 
 import "./Token.sol";
 
 contract TokenFactory {
-    event TokenCreated(address indexed tokenAddress, address indexed owner, uint256 initialSupply,string name,string symbol);
+    event TokenCreated(address tokenAddress, uint256 initialSupply,string name,string symbol);
 
-    struct TokenStruct{
-        address tokenAddress;
-        string name; 
-        string symbol;
-    }
+    constructor(){}
 
-    TokenStruct[] tokens;
-
-    function createToken(address initialOwner, uint256 initialSupply, string memory name, string memory symbol) public returns (address) {
+    function createToken(uint256 initialSupply, string memory name, string memory symbol) public returns (address) {
+        require(initialSupply > 0, "Initial supply must be greater than 0");
+        require(bytes(name).length > 0, "Token name is required");
+        require(bytes(symbol).length > 0, "Token symbol is required");
     
-        Token newToken = new Token(initialOwner, initialSupply, name, symbol);
+        Token newToken = new Token(msg.sender, initialSupply, name, symbol);
 
-        TokenStruct memory token = TokenStruct({
-            tokenAddress: address(newToken),
-            name: name,
-            symbol: symbol
-        });
-
-        tokens.push(token);
-
-        emit TokenCreated(address(newToken), initialOwner, initialSupply,name,symbol);
+        emit TokenCreated(address(newToken), initialSupply,name,symbol);
         
         return address(newToken);
         
